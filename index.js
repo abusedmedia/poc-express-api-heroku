e3b0dc485bf7c6894256837401d915879fd1ef03
abusedmedia <ff@todo.to.it>
2018-09-27 12:39:58 +0200

    added cors enabler

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 3131
+
+app.use(cors())
 
 app.get('/api', (req, res) => {
   res.status(200).json({api: 'version 1'})
